9026f859ccfb174652cd976d46a63598de2a68da
Konstantin <constantin8271@gmail.com>
2022-04-21 22:03:05 +0300

    task_16

---
+++ Task16.py
@@ -0,0 +1,17 @@
+# Задать список из n чисел последовательности (1+1n)n и вывести на экран их сумму
+
+import os
+import random
+os.system("cls")
+
+n = random.randint(6, 16)
+print(n)
+
+massive = []
+
+for i in range(1, n+1):
+    massive.append(1+(1/i)**i)
+print(f'{sum(massive):.2f}')
+
+
+
