dbfd54da6b035697aba00f37ff5c22d5d469226b
Konstantin <constantin8271@gmail.com>
2022-04-21 21:19:07 +0300

    task_13_fix2

--- Task13.py
+++ Task13.py
@@ -58,4 +58,24 @@ def text_find(a,b):
             i += 1
     return count
 
+exid()
+
+# str1= input('First string: ')
+# str2= input('Second string: ')
+
+str1 = 'Python free 777 $$$ python free python python python $$$ python'
+str2 = 'python free new $$$'
+
+str_list = str1.lower().split()
+substr_list = str2.lower().split()
+
+# print('String: ' + str1)
+# print('Substrings: ' + str2)
+# print()
+#
+# for i in substr_list:
+#     count = 0
+#     for j in str_list:
+#         count = str_list.count(i)
+#     print(f'{i} found {count} times.')
 
