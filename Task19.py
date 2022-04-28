e59ed641efb04a05cf02d6023989e0f52f134ce8
Konstantin <constantin8271@gmail.com>
2022-04-28 11:47:59 +0300

    task_19

---
+++ Task19.py
@@ -0,0 +1,18 @@
+# Реализовать алгоритм задания случайных чисел. Без использования встроенного генератора псевдослучайных чисел
+import os
+os.system("cls")
+import time
+
+x = time.time()
+timespot = str(x)
+ran = ''
+expo = int(input('Введите количество знаков в нужном Вам случайном числе: '))
+
+print(x)
+for i in range (-expo, 0):
+      ran = ran + timespot[i]
+print(ran)
+
+
+
+
